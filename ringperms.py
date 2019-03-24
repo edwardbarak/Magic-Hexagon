37c98c8339a4314ceaf7f6481c30db986375bb49
Edward Barak <edward.barak@gmail.com>
2019-03-24 14:36:23 -0400

    Created more efficient brute force. Remaking solver to solve outer ring permutations first, 2 pieces at a time.

---
+++ ringperms.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# from numba import njit
+from itertools import permutations
+from time import time
+import numpy as np
+
+elapsed = lambda start: print('\nTime: %s' % (time() - start))
+
+def testing(runtime=False):
+    if runtime: start = time()
+    allPieces = set(range(1,20))
+
+    try:    
+        # generate all possible permutations of pieces in pos 1,2,3
+        perms = np.array(tuple(permutations(allPieces, 3)), dtype=np.int8)
+        # generate all possible permutations of pieces in pos 1,2,3 where the sum of those pieces is 38
+        perms = perms[np.where(np.sum(perms, axis=1) == 38)]
+        # all possible permutations of pos 7, 12, appended to respective permutation sequence
+        perms = [[(np.append(perm, np.array(perm2, dtype=np.int8))) for perm2 in permutations(allPieces.difference(perm),2)] for perm in perms]
+        perms = np.array(perms)
+        # reshape 3d array to 2d, so each row is a valid permutation sequence
+        perms = perms.reshape(perms.shape[0] * perms.shape[1], perms.shape[2])
+        # all possible permutations of pos 7, 12 where the sum of pos 3, 7, & 12 is 38
+        perms = perms[np.where(perms[:,2:].sum(axis=1) == 38)]
+        return perms
+    except KeyboardInterrupt:
+        pass
+
+    if runtime: print('\nTime: %s' % (time() - start))
+
+def solve(runtime=False):
+    if runtime: start = time()
+    allPieces = np.arange(1,20)
+    
+    try:    
+        # generate all possible permutations of pieces in pos 1,2,3
+        perms = np.array(tuple(permutations(allPieces, 3)), dtype=np.int8)
+        # only keep permutations where pieces in pos 1,2,3 sum up to 38
+        perms = perms[np.where(np.sum(perms, axis=1) == 38)]
+        print(perms.shape)
+        # find valid pieces for the following positions, two at a time, in sequential order: ((7,12), (16,19), (18,17), (13,8))
+        for i in range(4):
+            # get all possible permutations for next 2 positions, appeneded to their respective sequences
+            perms = [[np.append(perm, np.array(newperm, dtype=np.int8)) for newperm in permutations(np.setdiff1d(allPieces, perm), 2)] for perm in perms]
+            perms = np.array(perms)
+            # print(perms.shape)
+            # tperms = perms
+            # flatten 3d array to 2d array
+            perms = perms.reshape(np.prod(perms.shape[:2]), perms.shape[2])
+            # only keep permutations where the final 3 values in sequence sum up to 38            
+            perms = perms[np.where(perms[:,-3:].sum(axis=1) == 38)]
+            print(perms.shape)
+            
+
+        return perms
+        # # all possible permutations of pos 7, 12, appended to respective permutation sequence
+        # perms = [[(np.append(perm, np.array(perm2, dtype=np.int8))) for perm2 in permutations(allPieces.difference(perm),2)] for perm in perms]
+        # perms = np.array(perms)
+        # # reshape 3d array to 2d, so each row is a valid permutation sequence
+        # perms = perms.reshape(perms.shape[0] * perms.shape[1], perms.shape[2])
+        # # all possible permutations of pos 7, 12 where the sum of pos 3, 7, & 12 is 38
+        # perms = perms[np.where(perms[:,2:].sum(axis=1) == 38)]
+        # return perms
+    except KeyboardInterrupt:
+        pass
+    # except IndexError:
+    #     return tperms
+
+    if runtime: elapsed(start)
