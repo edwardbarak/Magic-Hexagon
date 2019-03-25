1f0c6fc1d71d205cda22ce1a0c21ef70478d9602
Edward Barak <edward.barak@gmail.com>
2019-03-25 09:08:55 -0400

    cleaned up code

--- ringperms.py
+++ ringperms.py
@@ -1,33 +1,10 @@
 #!/usr/bin/env python3
 
-# from numba import njit
 from itertools import permutations
 from time import time
 import numpy as np
 
 elapsed = lambda start: print('\nTime: %s' % (time() - start))
-
-def testing(runtime=False):
-    if runtime: start = time()
-    allPieces = set(range(1,20))
-
-    try:    
-        # generate all possible permutations of pieces in pos 1,2,3
-        perms = np.array(tuple(permutations(allPieces, 3)), dtype=np.int8)
-        # generate all possible permutations of pieces in pos 1,2,3 where the sum of those pieces is 38
-        perms = perms[np.where(np.sum(perms, axis=1) == 38)]
-        # all possible permutations of pos 7, 12, appended to respective permutation sequence
-        perms = [[(np.append(perm, np.array(perm2, dtype=np.int8))) for perm2 in permutations(allPieces.difference(perm),2)] for perm in perms]
-        perms = np.array(perms)
-        # reshape 3d array to 2d, so each row is a valid permutation sequence
-        perms = perms.reshape(perms.shape[0] * perms.shape[1], perms.shape[2])
-        # all possible permutations of pos 7, 12 where the sum of pos 3, 7, & 12 is 38
-        perms = perms[np.where(perms[:,2:].sum(axis=1) == 38)]
-        return perms
-    except KeyboardInterrupt:
-        pass
-
-    if runtime: print('\nTime: %s' % (time() - start))
 
 def solve(runtime=False):
     if runtime: start = time()
@@ -37,8 +14,7 @@ def solve(runtime=False):
         # generate all possible permutations of pieces in pos 1,2,3
         perms = np.array(tuple(permutations(allPieces, 3)), dtype=np.int8)
         # only keep permutations where pieces in pos 1,2,3 sum up to 38
-        perms = perms[np.where(np.sum(perms, axis=1) == 38)]
-        print(perms.shape)
+        perms = perms[np.where(np.sum(perms, axis=1) == 38)]        
         # find valid pieces for the following positions, two at a time, in sequential order: ((7,12), (16,19), (18,17), (13,8))
         for i in range(4):
             # get all possible permutations for next 2 positions, appeneded to their respective sequences
