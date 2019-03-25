278a7b526360d175ceb614cea6d9bfdd6cb4c911
Edward Barak <edward.barak@gmail.com>
2019-03-25 09:06:18 -0400

    refactored calculation of permutation pairs on the outer ring to increase performance.

--- ringperms.py
+++ ringperms.py
@@ -31,7 +31,7 @@ def testing(runtime=False):
 
 def solve(runtime=False):
     if runtime: start = time()
-    allPieces = np.arange(1,20)
+    allPieces = np.arange(1,20, dtype=np.int8)
     
     try:    
         # generate all possible permutations of pieces in pos 1,2,3
@@ -42,35 +42,21 @@ def solve(runtime=False):
         # find valid pieces for the following positions, two at a time, in sequential order: ((7,12), (16,19), (18,17), (13,8))
         for i in range(4):
             # get all possible permutations for next 2 positions, appeneded to their respective sequences
-            # TODO: Optimize line below. 95% of runtime spent here!!
-            # NOTE: for perm in perms, get setdiff; get permutations(setdiff,2); ...
-            # NOTE: locs = np.where(np.array(perm[-1] + permutations).sum() == 38); ...
-            # NOTE: combined = np.crossProduct(perms[locs], permutations[locs]); combined.reshape(2d);
-            perms = [[np.append(perm, np.array(newperm, dtype=np.int8)) for newperm in permutations(np.setdiff1d(allPieces, perm), 2)] for perm in perms]
-            perms = np.array(perms)
-            # print(perms.shape)
-            # tperms = perms
-            # flatten 3d array to 2d array
+            # get all possible permutations of perms for one of the pairs of positions mentioned above
+            _newperms = np.array([tuple(permutations(np.setdiff1d(allPieces, perm), 2)) for perm in perms], dtype=np.int8)
+            # merge perms with their new respective permutations
+            perms = np.array([np.repeat([perm], _newperms.shape[1], axis=0) for perm in perms])
+            perms = np.append(perms, _newperms, axis=2)
             perms = perms.reshape(np.prod(perms.shape[:2]), perms.shape[2])
-            # only keep permutations where the final 3 values in sequence sum up to 38            
+            # only keep permutations where the last 3 calculated pieces sum up to 38
             perms = perms[np.where(perms[:,-3:].sum(axis=1) == 38)]
-            print(perms.shape)
             
         # find piece for position 4
         # TODO: if 38 - perm[[0,-1]].sum() in setdiff then add perm to perms
 
-        return perms
-        # # all possible permutations of pos 7, 12, appended to respective permutation sequence
-        # perms = [[(np.append(perm, np.array(perm2, dtype=np.int8))) for perm2 in permutations(allPieces.difference(perm),2)] for perm in perms]
-        # perms = np.array(perms)
-        # # reshape 3d array to 2d, so each row is a valid permutation sequence
-        # perms = perms.reshape(perms.shape[0] * perms.shape[1], perms.shape[2])
-        # # all possible permutations of pos 7, 12 where the sum of pos 3, 7, & 12 is 38
-        # perms = perms[np.where(perms[:,2:].sum(axis=1) == 38)]
-        # return perms
+        
     except KeyboardInterrupt:
         pass
-    # except IndexError:
-    #     return tperms
 
     if runtime: elapsed(start)
+    return perms
