2fcfb7f786a26897e6ed2a318139ef4e76881f76
Edward Barak <edward.barak@gmail.com>
2019-03-25 21:04:22 -0400

    Refactored the rest of solve()

--- ringperms.py
+++ ringperms.py
@@ -16,53 +16,45 @@ def solve(runtime=True):
     try:    
         # OUTER RING CALCULATIONS
         # Generate all possible choices for positions 1, 2, & 3
-        _newperms = permutations(allPieces, 3)
+        _newperms = permutations(allPieces, 3)                                                  # Generation
         perms = np.array(tuple(_newperms), dtype=np.int8)
         # Filter out choices where where pieces in positions 1, 2, & 3 do not sum up to 38
-        perms = perms[np.where(np.sum(perms, axis=1) == 38)]        
+        perms = perms[np.where(np.sum(perms, axis=1) == 38)]                                    # Filtration        
         
         # Find the remaining pieces for the outer ring
         for i in range(4):
-            # Find possible choices for the next 2 pieces
+            # Find possible choices for next 2 pieces in the outer ring
             _newperms = np.array([tuple(permutations(np.setdiff1d(allPieces, perm), 2)) for perm in perms], dtype=np.int8)
-            perms = np.array([np.repeat([perm], _newperms.shape[1], axis=0) for perm in perms]) # Transform
-            perms = np.append(perms, _newperms, axis=2)                                         # Transform
-            perms = perms.reshape(np.prod(perms.shape[:2]), perms.shape[2])                     # Transform
-            perms = perms[np.where(perms[:,-3:].sum(axis=1) == 38)]                             # Filter
+            perms = np.array([np.repeat([perm], _newperms.shape[1], axis=0) for perm in perms]) # Transformation
+            perms = np.append(perms, _newperms, axis=2)                                         # Transformation
+            perms = perms.reshape(np.prod(perms.shape[:2]), perms.shape[2])                     # Transformation
+            perms = perms[np.where(perms[:,-3:].sum(axis=1) == 38)]                             # Filtration
             
         # Find the final piece of the outer ring
-        _pos4diffs = np.subtract(38, perms[:,[0,-1]].sum(axis=1)).astype(np.int8)
-        # check if pos4 <= 19, since no piece larger than 19 exists
-        _isBetween1and19 = _pos4diffs <= 19
-        # all perms and pos4 where pos4 <= 19,
-        _pos4diffs = _pos4diffs[np.where(_isBetween1and19)]
-        perms = perms[np.where(_isBetween1and19)]
-        perms = np.append(perms, _pos4diffs[:,None], axis=1)
-        # all perms where the piece in pos4 hasn't already been used in a different pos
-        _pos4notUsed = np.array([perm[-1] not in perm[:-1] for perm in perms])
-        perms = perms[np.where(_pos4notUsed)]
+        _differences = np.subtract(38, perms[:,[0,-1]].sum(axis=1)).astype(np.int8)             # Subtraction
+        _isValidPiece = _differences <= 19                                                      # Validation
+        _differences = _differences[np.where(_isValidPiece)]                                    # Filtration        
+        perms = perms[np.where(_isValidPiece)]                                                  # Filtration
+
+        perms = np.append(perms, _differences[:,None], axis=1)                                  # Transformation
+        _isValidPiece = np.array([perm[-1] not in perm[:-1] for perm in perms])                 # Transformation
+        perms = perms[np.where(_isValidPiece)]                                                  # Filtration
 
         # INNER RING CALCULATIONS
-        # solve for pos5, pos9
-        # _perms for pos5, pos9
-        # get leftover pieces for each sequence
-        _diff_func = lambda x: np.setdiff1d(allPieces, x)
-        _diff = np.apply_along_axis(_diff_func, 1, perms)
-        # get permutations of leftover pieces
-        _getNewPerms = lambda x: np.array(tuple(permutations(x, 2)), dtype=np.int8)
-        _newperms = np.apply_along_axis(_getNewPerms, 1, _diff)
-        # append _newperms to perms
-        perms = np.repeat(perms, _newperms.shape[1], axis=0)
-        _newperms = _newperms.reshape(np.prod(_newperms.shape[:2]), _newperms.shape[2])
-        perms = np.append(perms, _newperms, axis=1)
-        # keep all perms where 38 - sum(pos2,pos13) == pos5 + pos9
-        # pos2 = perm[1], pos13 = perm[9] 
-        perms = perms[np.where(38 - perms[:,[1, 9]].sum(axis=1) == perms[:,-2:].sum(axis=1))]        
-        # get pos6
-        # where 38 - sum(pos4, pos5, pos7) in setdiff(allPieces) 
-        # pos4 = perm[11], pos5 = perm[13], pos7 = perm[3]
+        # Find the first two pieces of the inner ring
+        _get_difference = lambda x: np.setdiff1d(allPieces, x)
+        _differences = np.apply_along_axis(_get_difference, 1, perms)                           # Subtraction
         
-        # Solve for positions 6, 11, 14, & 15
+        _getNewPerms = lambda x: np.array(tuple(permutations(x, 2)), dtype=np.int8)
+        _newperms = np.apply_along_axis(_getNewPerms, 1, _differences)                          # Generation
+        
+        perms = np.repeat(perms, _newperms.shape[1], axis=0)                                    # Transformation
+        _newperms = _newperms.reshape(np.prod(_newperms.shape[:2]), _newperms.shape[2])         # Transformation
+        perms = np.append(perms, _newperms, axis=1)                                             # Transformation
+
+        perms = perms[np.where(38 - perms[:,[1, 9]].sum(axis=1) == perms[:,-2:].sum(axis=1))]   # Filtration        
+        
+        # Find the remaining pieces of the outer ring
         placeDifference = lambda perms, x, y, z: np.append(perms, (38 - perms[:,[x,y,z]].sum(axis=1))[:,None], axis=1)
         placeDifferenceValidation = lambda perms: perms[np.where(np.array([np.unique(perm).shape == perm.shape for perm in perms], dtype=np.int8))]
                 
@@ -74,12 +66,13 @@ def solve(runtime=True):
             ]
         
         for i in dependents:
-            perms = placeDifference(perms, i[0], i[1], i[2])
-            perms = placeDifferenceValidation(perms)
+            perms = placeDifference(perms, i[0], i[1], i[2])                                    # Subtraction
+            perms = placeDifferenceValidation(perms)                                            # Validation
 
-        # Solve for position 10
-        perms = np.append(perms, np.array([np.setdiff1d(allPieces, perm)[0] for perm in perms], dtype=np.int8)[:,None], axis=1)
-        results = perms[np.nonzero(test(perms, False, False))]
+        # Find the centeral piece (last piece)
+        _differences = [np.setdiff1d(allPieces, perm)[0] for perm in perms]                     # Subtraction
+        perms = np.append(perms, np.array(_differences, dtype=np.int8)[:,None], axis=1)         # Transformation
+        results = perms[np.nonzero(test(perms, False, False))]                                  # Filtration
         
     except KeyboardInterrupt:
         if runtime: elapsed(start)        
